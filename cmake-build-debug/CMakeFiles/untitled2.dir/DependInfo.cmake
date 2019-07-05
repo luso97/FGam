fbdc8f2d03fc91a37fe3716408d308707f5669a6
luso97 <mcluis1997@gmail.com>
2019-07-05 11:52:29 +0200

    Added SQLite and Qt libraries to CMake

---
+++ cmake-build-debug/CMakeFiles/untitled2.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Luis/CLionProjects/untitled2/main.cpp" "C:/Users/Luis/CLionProjects/untitled2/cmake-build-debug/CMakeFiles/untitled2.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Qt/5.12.1/mingw73_64/include"
+  "C:/Qt/5.12.1/mingw73_64/include/QtWidgets"
+  "C:/Qt/5.12.1/mingw73_64/include/QtGui"
+  "C:/Qt/5.12.1/mingw73_64/include/QtANGLE"
+  "C:/Qt/5.12.1/mingw73_64/include/QtCore"
+  "C:/Qt/5.12.1/mingw73_64/./mkspecs/win32-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
